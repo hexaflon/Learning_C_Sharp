ec7596b8882222ebadceac21d79e816aaa3e1c53
Hexa <Hexa@DESKTOP-933JN4D>
2022-07-22 16:04:01 +0200

    Dodaj pliki projektów.

---
+++ Second/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2;
+            Console.WriteLine(a);
+            //comments
+            /*
+             *this 
+             *is
+             *comment
+             */
+            Console.WriteLine("\tBros");
+            Console.ReadKey();
+        }
+    }
+}
